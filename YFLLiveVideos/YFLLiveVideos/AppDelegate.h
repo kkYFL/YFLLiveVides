8a5165be4cb85f2f896c750c8626977ac2a00ef4
杨丰林 <yangfenglin@yangfenglindeiMac.local>
2017-06-19 10:27:50 +0800

    初始化项目

---
+++ YFLLiveVideos/YFLLiveVideos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFLLiveVideos
+//
+//  Created by 杨丰林 on 2017/6/19.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFLLiveVideos/YFLLiveVideos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFLLiveVideos
+//
+//  Created by 杨丰林 on 2017/6/19.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
